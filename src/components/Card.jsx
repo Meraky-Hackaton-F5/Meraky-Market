c77eceb9c3ed7c062a04dc900e27a3de39b42320
Mónica R <a20monicarr@gmail.com>
2023-02-23 14:13:29 +0100

    added commit

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,38 +3,23 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import "./Card.css"
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import db from '../../api/db.json'
-import { Link } from 'react-router-dom';
-
+import perros from "../assets/perros.png"
 
 function Cards() {
-  const data = db.products;
-
   return (
     <Card style={{ width: '18rem' }}>
-      {data.map(product => {
-        return (
-          <div key={product.id}>
-            <Card.Img variant="top" src={product.img} />
-            <Card.Body>
-              <Card.Title>{product.title}</Card.Title>
-              <Card.Text>
-                {product.location}
-              </Card.Text>
-              {
-
-                    <Link to={`/advertisement/${product.id}`}>
- <Button variant="primary">Quiero saber más</Button>
-</Link>}
-
-
-              
-            </Card.Body>
-          </div>
-        )
-      })}
+      <Card.Img variant="top" src={perros} />
+      <Card.Body>
+        <Card.Title>Card Title</Card.Title>
+        <Card.Text>
+          Some quick example text to build on the card title and make up the
+          bulk of the card's content.
+        </Card.Text>
+        <Button variant="primary">Leer más</Button>
+      </Card.Body>
     </Card>
   );
 }
 
 export default Cards;
+
