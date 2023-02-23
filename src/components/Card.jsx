7b95a414a1aceb99683e5776c6e6956d70275d9c
Mariovalcen <mariovalcen@gmail.com>
2023-02-23 11:35:18 +0100

    Database mapping for cards

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,23 +3,29 @@ import 'bootstrap/dist/css/bootstrap.min.css'
 import "./Card.css"
 import Button from 'react-bootstrap/Button';
 import Card from 'react-bootstrap/Card';
-import perros from "../assets/perros.png"
+import db from '../../api/db.json'
 
 function Cards() {
+  const data = db.products;
+
   return (
     <Card style={{ width: '18rem' }}>
-      <Card.Img variant="top" src={perros} />
-      <Card.Body>
-        <Card.Title>Card Title</Card.Title>
-        <Card.Text>
-          Some quick example text to build on the card title and make up the
-          bulk of the card's content.
-        </Card.Text>
-        <Button variant="primary">Go somewhere</Button>
-      </Card.Body>
+      {data.map(product => {
+        return (
+          <div key={product.id}>
+            <Card.Img variant="top" src={product.img} />
+            <Card.Body>
+              <Card.Title>{product.title}</Card.Title>
+              <Card.Text>
+                {product.location}
+              </Card.Text>
+              <Button variant="primary">Go somewhere</Button>
+            </Card.Body>
+          </div>
+        )
+      })}
     </Card>
   );
 }
 
 export default Cards;
-
